0f2c5e78921bd519e7f8f01aabf8715e097ea093
Travis Alstrand <travisalstrand@gmail.com>
2023-07-14 13:38:17 -0700

    Complete overhaul

--- 1-Starter_Files/script.js
+++ 1-Starter_Files/script.js
@@ -6,58 +6,59 @@
 
 // 2: select the breeds select element and store it in a variable
 
-// 3: select all of the option elements with a [data-theme] and store it in a variable
-
+// 3: select all of the option elements with a [data-theme] attribute and store them in a variable
 
 // 4: add an event listener on the animals select element that listens for the "change" event
 
-// 5: capture the value of the animals select element and store it in a variable
+//   5: start a loop to iterate over each breed option
 
-// 6: start a loop to iterate over each breed option
+//     6: check if the animals select value is not equal to the current breed option's data-breed attribute value
 
-  // 7: check if the animals select value is not equal to the current breed option's data-breed attribute value
+//       7: if they are not equal, hide the current breed option
 
-  // 8: if they are not equal, hide the current breed option
+//       8: if they are equal, do not hide (or show) the current breed option
 
-  // 9: if they are equal, do not hide the current breed option
+//   9. check if the breeds select value is NOT equal to an empty string
 
-// 10: reset the breeds select element's selected option to it's first child element
-//     upon each animal "change". This will prevent a mix-match of animals / breeds.
+//     10. if it's not an empty string, set the breeds select value to the second option's value
 
-// TEST-HINT: Select an animal, then a breed. Then change the animal again...
-
-/* ==== ENTER YOUR CODE BELOW ==== */
-
-
-
-
-
-
-
+// TESTING TIP: Select an animal, select a matching breed, then change
+// the animal again, see if the breed also changes to "Please choose again"
 
 //==============================================================================//
 //====================DON'T CHANGE THE CODE BELOW===============================//
 //==============================================================================//
-const form = document.getElementById("form");
-const errorMessage = document.getElementById("error-message");
-const successMessage = document.getElementById("success-message");
-successMessage.style.display = "none";
-errorMessage.style.display = "none";
+const form = document.querySelector("#form");
+const message = document.querySelector("#message");
+message.style.display = "none";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  successMessage.style.display = "none";
-  errorMessage.style.display = "none";
-  const isDogAnimal = document.getElementById("animals").value.includes("Dogs");
-  const isDogBreed = document.getElementById("breeds").value.includes("dog");
-  const isCatAnimal = document.getElementById("animals").value.includes("Cats")
-  const isCatBreed = document.getElementById("breeds").value.includes("cat");
 
-  if ((isDogAnimal && isDogBreed) || (isCatAnimal && isCatBreed)) {
-    successMessage.style.display = "inherit";
-    errorMessage.style.display = "none";
+  const isMatch =
+    (document.querySelector("#animals").value.includes("Dogs") &&
+      document.querySelector("#breeds").value.includes("Dogs")) ||
+    (document.querySelector("#animals").value.includes("Cats") &&
+      document.querySelector("#breeds").value.includes("Cats"));
+
+  if (isMatch) {
+    updateMessage("WOOHOO! The animal / breeds match!", isMatch);
+    message.style.display = "inherit";
   } else {
-    successMessage.style.display = "none";
-    errorMessage.style.display = "inherit";
+    updateMessage("OOPS! The animal / breeds don't match!", isMatch);
+    message.style.display = "inherit";
   }
 });
+
+function updateMessage(text, isMatch) {
+  message.className = "";
+  message.textContent = text;
+  if (isMatch) {
+    message.classList.add("success");
+  } else {
+    message.classList.add("error");
+  }
+
+  message.offsetHeight;
+  message.classList.add("message");
+}
--- 2-Completed_Files/script.js
+++ 2-Completed_Files/script.js
@@ -1,71 +1,68 @@
 // select the animals select element and store it in a variable
-const animalSelect = document.getElementById("animals");
+const animalSelect = document.querySelector("#animals");
 // select the breeds select element and store it in a variable
-const breedSelect = document.getElementById("breeds");
-// select all of the option elements with a [data-theme] and store it in a variable
+const breedSelect = document.querySelector("#breeds");
+// select all of the option elements with a [data-theme] attribute and store them in a variable
 const breedOptions = document.querySelectorAll("option[data-breed]");
 
-// 1: add an event listener on the animals select element
-//    that listens for a "change" event
-// 2: capture the value of the animals select element and store it in a variable
-// 3: start a loop to iterate over each breed option
-// 4: check if the animals select value is not equal to the current breed option's
-//    data-breed attribute value
-// 5: if they are not equal, hide the current breed option
-// 6: if they are equal, do not hide the current breed option
-// 7: reset the breeds select element's selected option to it's first child element
-//   upon each animal "change". This will prevent a mix-match of animals / breeds.
-
-// HINT: Select an animal, then a breed. Then change the animal again...
-
+// 1: add an event listener on the animals select element that listens for a "change" event
 animalSelect.addEventListener("change", () => {
-
-  const animal = animalSelect.value;
-
-  breedSelect.firstElementChild.selected = true;
-
-  breedOptions.forEach(option => {
-    if (animal !== option.getAttribute("data-breed")) {
+  // 2: start a loop to iterate over each breed option
+  breedOptions.forEach((option) => {
+    // 3: check if the animals select value is NOT equal to the current breed option's [data-breed] attribute value
+    if (animalSelect.value !== option.getAttribute("data-breed")) {
+      // 4: if they are not equal, hide the current breed option
       option.hidden = true;
     } else {
+      // 5: if they are equal, unhide (or show) the current breed option
       option.hidden = false;
     }
   });
 
-  // ====== FOR LOOP EQUIVALENT ====== //
-
-  // for(let i = 0; i < breedOptions.length; i++) {
-  //   if (animal !== breedOptions[i].getAttribute('data-breed')) {
-  //     breedOptions[i].hidden = true;
-  //   } else {
-  //     breedOptions[i].hidden = false;
-  //   }
-  // }
+  // 6. check if the breeds select value is NOT equal to an empty string
+  if (breedSelect.value !== "") {
+    // 7. if step 6 is true, set the breeds select value to the second option's value
+    breedSelect.value = "reselect";
+  }
 });
+
+// TESTING TIP: Select an animal, select a matching breed, then change
+// the animal again, see if the breed also changes to "Please choose again"
 
 //==============================================================================//
 //====================DON'T CHANGE THE CODE BELOW===============================//
 //==============================================================================//
-const form = document.getElementById("form");
-const errorMessage = document.getElementById("error-message");
-const successMessage = document.getElementById("success-message");
-successMessage.style.display = "none";
-errorMessage.style.display = "none";
+const form = document.querySelector("#form");
+const message = document.querySelector("#message");
+message.style.display = "none";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  successMessage.style.display = "none";
-  errorMessage.style.display = "none";
-  const isDogAnimal = document.getElementById("animals").value.includes("Dogs");
-  const isDogBreed = document.getElementById("breeds").value.includes("dog");
-  const isCatAnimal = document.getElementById("animals").value.includes("Cats")
-  const isCatBreed = document.getElementById("breeds").value.includes("cat");
 
-  if ((isDogAnimal && isDogBreed) || (isCatAnimal && isCatBreed)) {
-    successMessage.style.display = "inherit";
-    errorMessage.style.display = "none";
+  const isMatch =
+    (document.querySelector("#animals").value.includes("Dogs") &&
+      document.querySelector("#breeds").value.includes("Dogs")) ||
+    (document.querySelector("#animals").value.includes("Cats") &&
+      document.querySelector("#breeds").value.includes("Cats"));
+
+  if (isMatch) {
+    updateMessage("WOOHOO! The animal / breeds match!", isMatch);
+    message.style.display = "inherit";
   } else {
-    successMessage.style.display = "none";
-    errorMessage.style.display = "inherit";
+    updateMessage("OOPS! The animal / breeds don't match!", isMatch);
+    message.style.display = "inherit";
   }
 });
+
+function updateMessage(text, isMatch) {
+  message.className = "";
+  message.textContent = text;
+  if (isMatch) {
+    message.classList.add("success");
+  } else {
+    message.classList.add("error");
+  }
+
+  message.offsetHeight;
+  message.classList.add("message");
+}
